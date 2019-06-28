4e65fdcb390c08492ae70901b10674d3229ba22b
Marc-Antoine Dubé <marc-antoine.dube@bnc.ca>
2019-06-28 16:41:48 -0400

    asdf

--- src/main/java/com/atomist/spring/SpringRestSeedController.java
+++ src/main/java/com/atomist/spring/SpringRestSeedController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 class SpringRestSeedController {
 
-    @GetMapping("/hello/{name1}")
-    public String person(@PathVariable String name1) {
-        return "Hello " + name1 + "!";
+    @GetMapping("/hello/{name}")
+    public String person(@PathVariable String name) {
+        return "Hello " + name + "!";
     }
 
 }
